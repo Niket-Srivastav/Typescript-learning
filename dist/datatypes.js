e192188948604ce9742726d7f2a9d296df52cc4b
Niket-Srivastav <saketabc111@gmail.com>
2025-01-27 21:55:52 +0530

    datatypes

--- dist/datatypes.js
+++ dist/datatypes.js
@@ -1,2 +1,24 @@
 "use strict";
-let a = 5;
+let My_name = "Niket";
+let newname = My_name.toUpperCase();
+console.log(newname);
+let age = 24;
+age = 25.5;
+age++;
+console.log(age);
+// other datatypes - 
+// - boolean
+// - array 
+let arr;
+let arr1;
+arr = ["a", "b", "c"];
+let result = arr.filter((x) => x === 'a');
+console.log(result);
+// enum
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+let c = Color.Green;
--- src/datatypes.ts
+++ src/datatypes.ts
@@ -1,1 +1,45 @@
-let a:number = 5;
+let My_name:string = "Niket";
+
+let newname  =  My_name.toUpperCase();
+
+console.log(newname);
+
+let age:number = 24;
+
+age = 25.5 
+age++;
+
+console.log(age)
+
+// other datatypes - 
+// - boolean
+// - array 
+
+let arr:string[];
+let arr1:Array<string>;
+
+arr = ["a","b","c"];
+
+let result = arr.filter((x) => x === 'a');
+
+console.log(result)
+
+
+// enum
+
+const enum Color{ //const will remove the the enum code from the js file
+  Red,
+  Green,
+  Blue
+}
+
+let c:Color = Color.Green;
+
+// tuple
+
+let swapNumbs: [firstnum:number, secondnum:number];
+
+swapNumbs = [10,20];
+
+// any
+
